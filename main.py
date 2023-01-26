4c3d125001d70d0cfdb1b2adc8553cda6671ec02
Evan McCaleb <evanmccaleb@gmail.com>
2023-01-25 22:51:28 -0500

    Basic user input and sanitization

--- main.py
+++ main.py
@@ -4,16 +4,30 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-#load the page at the given URL
+# load the page at the given URL
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+while True:
+    UserInput = input("Issue date YYYMMDD")
+    try:
+        assert len(UserInput) == 8
+        year = int(UserInput[0:4])
+        month = int(UserInput[4:6])
+        day = int(UserInput[6:])
+        assert 0 < month <= 12
+        assert 0 < day <= 31
+        break
+    except AssertionError:
+        print(" Try again YYYMMDD MM=01-12, DD=01-31 ")
+    except ValueError:
+        print("Try again enter only 0-9: YYMMDD")
 
 browser = webdriver.Firefox()
 browser.get('http://archive.aviationweek.com/')
 
 time.sleep(2)
 
-search=browser.find_element(By.XPATH,"//a[contains(text(),'Browse Issues')]")
+search = browser.find_element(By.XPATH, "//a[contains(text(),'Browse Issues')]")
 search.click()
